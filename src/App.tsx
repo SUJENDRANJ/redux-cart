814a330adaf0aa22bc97924e7abcd695bca978b8
Sujendran <sujendranjanagiraman@gmail.com>
2025-03-07 14:45:41 +0530

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/Navbar';
-import { HomePage } from './pages/HomePage';
-import { CartPage } from './pages/CartPage';
+import { Routes, Route, HashRouter } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { HomePage } from "./pages/HomePage";
+import { CartPage } from "./pages/CartPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="min-h-screen bg-gray-100">
         <Navbar />
         <Routes>
@@ -14,7 +13,7 @@ function App() {
           <Route path="/cart" element={<CartPage />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
